01ec7f73d2d1900d706f732712c90c451cd7f205
Mohammed Faseen <muhammedfaseen94@gmail.com>
2026-06-24 08:16:10 +0530

    code the functions to convert fahrenhiet to celsius and vice versa.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  let result = (degree - 32) * (5/9);
+  return +result.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  let result = (32 + ((9/5) * degree))
+  return +result.toFixed(1);
 };
 
 // Do not edit below this line
